baec21ef85b8183254dc1c1caebb37203e422b94
Gzeno <tser@rpi.edu>
2015-10-13 16:00:23 -0400

    Added HTML/CSS, added basic session code

--- server/app.js
+++ server/app.js
@@ -1,10 +1,31 @@
 // Server scriptor Node.js
 
 var express = require('express');
+var cookieParser = require('cookie-parser');
+//var bodyParser = require('body-parser');
+var session = require('cookie-session');
+
+
 var app = express();
+	
+//app.use(bodyParser.json());
+app.use(cookieParser('somekindofkey'));
+app.use(session({
+	name: 'session',
+	keys: ['key1', 'key2']
+}));
+app.use(express.static('res'));
+
+// Functions
+
+// get session name
+function getName(req, res) {
+	return
+}
+
 
 app.get('/', function(req,res){
-	res.send('hello world');
+	res.sendFile(__dirname+"/res/html/home.html");
 });
 
 app.listen(3000);
